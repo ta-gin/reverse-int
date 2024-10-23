783d5d0334170a9458b7ac4102eda3a7eb2c3515
ta-gin <tata.ergina@yandex.ru>
2024-10-23 20:52:07 +0300

    feat: add function for reverse numbers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const toString = String(Math.abs(n));
+    const reverseStringToNumber = Number(toString.split("").reverse().join(""));
+    return reverseStringToNumber;
+};
